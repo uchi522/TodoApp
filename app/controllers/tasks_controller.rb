1955d5b14e1c895c234a1d9a5beaa70e444a078f
uchi522 <sh-uchiyama@dgic.co.jp>
2020-01-20 04:27:11 +0000

    Initial commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,17 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def store
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/store'
+  get 'tasks/update'
+  get 'tasks/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
