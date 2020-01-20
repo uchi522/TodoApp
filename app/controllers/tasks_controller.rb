a4a2210b78e40473011981a280ff9a63f1de01a4
uchi522 <sh-uchiyama@dgic.co.jp>
2020-01-20 06:53:54 +0000

    プロトタイプrev1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,29 @@
+# frozen_string_literal: true
+
+# TasksController
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
 
-  def show
-  end
-
   def store
+    task = Task.new
+    task.title = params[:title]
+    task.status = 'todo'
+    task.save
+    redirect_to '/tasks', notice: 'タスクを作成しました。'
   end
 
   def update
+    task = Task.find(params[:id])
+    task.status = 'done'
+    task.save
+    redirect_to '/tasks', notice: 'タスク状態を更新しました。'
   end
 
   def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to '/tasks', notice: 'タスクを削除しました。'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/store'
-  get 'tasks/update'
-  get 'tasks/destroy'
+  get     'tasks'     => 'tasks#index'
+  post    'tasks'     => 'tasks#store'
+  put     'tasks/:id' => 'tasks#update'
+  delete  'tasks/:id' => 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
